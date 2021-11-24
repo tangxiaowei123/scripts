8a3f80bf774fbf948dbe6790eec2805e1c7c74b4
raysenlau <42733175+raysenlau@users.noreply.github.com>
2021-11-24 16:55:18 +0800

    添加：VirtualBox虚拟机Android编译环境配置

---
+++ android_build_env.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2018 Harsh 'MSF Jarvis' Shandilya
+# Copyright (C) 2018 Akhil Narang
+# SPDX-License-Identifier: GPL-3.0-only
+
+# Script to setup an AOSP Build environment on Ubuntu and Linux Mint
+
+LATEST_MAKE_VERSION="4.3"
+UBUNTU_16_PACKAGES="libesd0-dev"
+UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
+DEBIAN_10_PACKAGES="libncurses5"
+PACKAGES=""
+
+sudo apt update
+
+# Install lsb-core packages
+sudo apt install lsb-core -y
+
+LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
+
+if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
+    PACKAGES="${UBUNTU_16_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" ]]; then
+    PACKAGES="${UBUNTU_20_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
+    PACKAGES="${DEBIAN_10_PACKAGES}"
+fi
+
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install \
+    adb autoconf automake axel bc bison build-essential \
+    ccache clang cmake expat fastboot flex g++ \
+    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+    texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+    libxml-simple-perl apt-utils \
+    ${PACKAGES} -y
+
